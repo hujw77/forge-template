22177db15f5acc0d98975fb1b65b8b550d7619a8
echo <hujw77@gmail.com>
2023-10-25 15:58:54 +0800

    ci

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,8 +2,10 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
+import {Common} from "create3-deploy/script/Common.s.sol";
+import {ScriptTools} from "create3-deploy/script/ScriptTools.sol";
 
-contract Deploy is Script {
+contract Deploy is Common {
     function setUp() public {}
 
     function run() public {

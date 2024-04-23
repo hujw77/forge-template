9921407496344a3f63e333838298e459e89175c9
echo <hujw77@gmail.com>
2024-04-23 16:04:02 +0800

    clean

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,19 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
-import {ScriptTools} from "create3-deploy/script/ScriptTools.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract Deploy is Script {
-    function name() public pure override returns (string memory) {
-        return "Deploy";
-    }
-
-    function setUp() public override {
-        super.setUp();
+    modifier broadcast() {
+        vm.startBroadcast();
+        _;
+        vm.stopBroadcast();
     }
 
     function run() public broadcast {
         Counter counter = new Counter();
+        counter.increment();
     }
 }
